891283663cc4060149150f05f75cc18ae70e975f
ggapsang <58983068+ggapsang@users.noreply.github.com>
2023-07-26 17:38:03 +0900

    Update and rename phonbook_1.0.c to phonbook_2.0.c
    2.0 버전. save, load, 정렬 추가

--- phonbook_2.0.c
+++ phonbook_2.0.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 #include<string.h>
 
+#include<stdio.h>
+#include<string.h>
+
 #define CAPACITY 100
 #define BUFFER_SIZE 20
 
@@ -12,6 +15,8 @@ void add();
 void find();
 void status();
 void remove_del();
+void load();
+void save();
 
 int main(){
 	char command[BUFFER_SIZE];
@@ -27,6 +32,10 @@ int main(){
 			status();
 		else if (strcmp(command, "delete")==0)
 			remove_del();
+		else if (strcmp(command, "read")==0)
+			load();
+		else if (strcmp(command, "save")==0)
+			save();
 		else if (strcmp(command, "exit")==0)
 			break; 
 	}
@@ -83,4 +92,47 @@ void remove_del(){
 		}
 	}
 	printf("No person named '%s' exists. \n", buf);
+}
+
+void load(){
+	char fileName[BUFFER_SIZE];
+	char buf1[BUFFER_SIZE];
+	char buf2[BUFFER_SIZE];
+	
+	scanf("%s", fileName);
+	
+	FILE *fp = fopen(fileName, "r"); // "r"은 read mode//
+	if (fp==NULL){                      //파일이 존재하지 않을 경우// 
+		printf("Open failed. \n");
+		return;
+	}
+	
+	while ((fscanf(fp, "%s", buf1) != EOF)){  //fscanf = 파일을 읽기 위한 함수 EOF end of file// 
+		fscanf(fp, "%s", buf2);
+		names[n] = strdup(buf1);
+		numbers[n]= strdup(buf2);
+		n++;
+	}
+	
+	fclose(fp);
+	
+}
+
+void save(){
+	int i;
+	char fileName[BUFFER_SIZE];
+	char tmp[BUFFER_SIZE];
+	
+	scanf("%s", tmp);
+	scanf("%s", fileName);
+	
+	FILE *fp = fopen(fileName, "r"); // "w"은 write mode//
+	if (fp==NULL){                      //파일이 존재하지 않을 경우// 
+		printf("Open failed. \n");
+		return;
+	}
+	for (i=0; i<n; i++){
+		fprintf(fp, "%s %s\n", names[i], numbers[i]);
+	}
+	fclose(fp);
 }
